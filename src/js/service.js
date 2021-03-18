d8f9dd52cb79c96d1fd77e5d809efd61a2d00190
Laurie Waller <lauriewaller7@gmail.com>
2021-03-18 14:18:12 -0700

    creating working API call

--- src/js/service.js
+++ src/js/service.js
@@ -2,21 +2,29 @@ export default class DinoLand {
   static dinoParagraphs(paragraph, words) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
+
       const url = `http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=${paragraph}&words=${words}`;
+      
+
+      console.log(url);
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
+          console.log(request.response);
+
         } else {
           reject(request.response);
         }
-      }
+      };
+      console.log(request);
+
       request.open("GET", url, true);
       request.send();
     }); 
   }
 }
 
-
+//`http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=${paragraph}&words=${words}`;
 // export default class WeatherService {  
 //   static getWeather(city) {
 //     return new Promise(function(resolve, reject) {
--- src/main.js
+++ src/main.js
@@ -1,29 +1,49 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import DinoLand from "./js/service.js";
 
-// $(document).ready(function() {
-//   $('#weatherLocation').click(function() {
-//     const city = $('#location').val();
-//     $('#location').val("");
+$(document).ready(function() {
+  $("#dino-paragraphs").click(function(event) {
+    event.preventDefault();
 
-//     let request = new XMLHttpRequest();
-//     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    let paragraph = $("input#para-number").val();
+    console.log(paragraph);
+    let words = $("input#word-number").val();
+    console.log(words);
 
-//     request.onreadystatechange = function() {
-//       if (this.readyState === 4 && this.status === 200) {
-//         const response = JSON.parse(this.responseText);
-//         getElements(response);
-//       }
-//     }
+    let promise = DinoLand.dinoParagraphs(paragraph, words);
+    promise.then(function(response) {
+      console.log(response);
+      // const body = JSON.parse(response);
+      // console.log(body);
+      $('.showDino').text(`${response}`);
 
-//     request.open("GET", url, true);
-//     request.send();
-
-//    function getElements(response) {
-//       $('.showHumidity').text(`The humidity in ${city} is ${response.clouds.all}%`);
-//       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-//     }
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+  });
+});
+// $('#weatherLocation').click(function() {
+//   let city = $('#location').val();
+//   clearFields();
+//   let promise = WeatherService.getWeather(city);
+//   promise.then(function(response) {
+//     const body = JSON.parse(response);
+//     $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+//     $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+//   }, function(error) {
+//     $('.showErrors').text(`There was an error processing your request: ${error}`);
 //   });
 // });
+
+
+
+
+// function clearFields() {
+//   $('#location').val("");
+//   $('.showErrors').text("");
+//   $('.showHumidity').text("");
+//   $('.showTemp').text("");
+// }
